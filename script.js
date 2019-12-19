c3e1f8e6f643c8d9fcc590ac2cc9fb97610bbb82
Alan Elam <aelam757@gmail.com>
2019-12-18 20:43:29 -0500

    final push

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
-// generate random password
-function generate() {
+var lowerCase = confirm("Do you want to use lowercase characters?");
+var upperCase= confirm("Do you want to use uppercase characters?");
+var numbers = confirm("Do you want to use numbers?");
+var symbols = confirm("Do you want to use symbols?");
 
-    //set password length
-    let complexity = document.getElementById("display")
+console.log(lowerCase);
+console.log(upperCase);
+console.log(numbers);
+console.log(symbols);
 
-    //possible password values
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+"
+function generate(){
 
-    let password = "";
 
-    //create for loop to choose password
-    for (var i = 0; i <= complexity; i++) {
+    var complexity = document.getElementById("slider").value;
+
+
+    var values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
+    var password = "";
+
+    for(var i = 0; i <= complexity; i++){
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-        else ()
     }
 
-    //add password to textbox/display
-    document.getElementById("display").values = password;
+
+    document.getElementById("display").value = password;
+
+}
+
+
+document.getElementById("slider").oninput = function(){
+
+    if(document.getElementById("slider").value > 0){
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+    }
+    else{
+        document.getElementById("length").innerHTML = "Length: 1";
+    }
+
+}
+
+function copyPassword(){
+
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+
+    alert("Password saved!");
 
 }
